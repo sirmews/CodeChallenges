d91b1bb04f0e361ca477c8ba60049b7b3fd375aa
Navishkar Rao <nav@navrao.me>
2023-04-15 10:32:14 +0000

    feat: basic input and add item

--- item-list-with-features/src/ItemList.jsx
+++ item-list-with-features/src/ItemList.jsx
@@ -1,3 +1,45 @@
+import {useState} from 'react'
+import {Item} from './Item'
+
 export const ItemList = ({ data }) => {
-  return null
+  const [items, setItems] = useState([]);
+  const [newItemText, setNewItemText] = useState("");
+
+  /**
+   * 
+   * @param {React.FormEvent} e 
+   */
+  const handleNewItemSubmit = (e) => {
+    // stop default
+    e.preventDefault();
+    setItems([...items, newItemText]);
+    // set to empty string again
+    setNewItemText("");
+  };
+
+
+  return (
+    <div className="container mt-5">
+      {items.map((item, index) => (
+        <p>Item goes here</p>
+      ))}
+      <hr/>
+      <form onSubmit={handleNewItemSubmit}>
+        <div className="field is-grouped">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter new item"
+              value={newItemText}
+              onChange={(e) => setNewItemText(e.target.value)}
+            />
+          </div>
+          <div className="control">
+            <button className="button is-primary">Add</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
